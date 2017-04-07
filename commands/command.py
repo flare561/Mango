feeec92aa6cefecaf28059960c5fe0248bb3b079
flare561 <flare561@gmail.com>
2017-04-07 01:12:34 -0500

    modified the command decorator to be simpler

--- commands/command.py
+++ commands/command.py
@@ -3,6 +3,8 @@ Module for registering commands within the package
 
 Author: flare561
 """
+from functools import partial
+
 registered_commands = {}
 __all__ = ["registered_commands", "command"]
 
@@ -17,16 +19,20 @@ class Command:
         return self.function(*args, **kwargs)
 
     def __repr__(self):
-        return (f"{self.function} nsfw: {self.nsfw} "
-                f"hidden: {self.hidden} help: {self.help}")
+        return (f"\"{self.function} nsfw: {self.nsfw} "
+                f"hidden: {self.hidden} help: {self.help}\"")
 
     @property
     def help(self):
         return self.function.__doc__
 
 
-def command(name, *, nsfw=False, hidden=False):
-    def wrapper(f):
-        registered_commands[name] = Command(f, nsfw=nsfw, hidden=hidden)
-        return f
-    return wrapper
+def command(func=None, *, name=None, nsfw=False, hidden=False):
+    if func is None:
+        return partial(command, name=name, nsfw=nsfw, hidden=hidden)
+
+    if name is None:
+        name = func.__name__
+
+    registered_commands[name] = Command(func, nsfw=nsfw, hidden=hidden)
+    return func
--- commands/echo.py
+++ commands/echo.py
@@ -1,7 +1,13 @@
 from .command import command
 
 
-@command("echo")
+@command(name="echo", nsfw=False, hidden=True)
 def echo(target, text, server):
     """Usage ~echo <text> Repeats what you said"""
     print(target, text)
+
+
+@command(nsfw=True)
+def sneko(target, text, server):
+    """Usage ~sneko Prints hiss"""
+    print(target, "hiss")
